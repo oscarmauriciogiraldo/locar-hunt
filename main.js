3e6991f957a210b4c51bf1633eaaa10b9123b13f
OscarGiraldo <oscar.giraldo@linktic.com>
2025-03-02 18:59:00 -0500

    feat: text animation position

--- main.js
+++ main.js
@@ -1,39 +1,12 @@
-import AFRAME from 'aframe'
-AFRAME.registerComponent('desaparecer-al-tocar', {
-    init: function () {
-      console.log('Objeto: cofre registrado')
-
-      let el = this.el;
-      let escalaInicial = el.getAttribute('scale');
-
-      let textoGanaste = document.getElementById('ganaste-text');
-      //let nuevoModelo = document.getElementById('nuevoModelo');
-
-      //Variable de control booleana
-      let tesoroRecogido = false;
-
-      el.addEventListener('click', function () {
-        console.log('objeto tocado')
-
-        if (!this.tesoroRecogido) {
-          this.tesoroRecogido = true;
-          console.log("Tesoro recogido:", this.tesoroRecogido);
-
-          // Animación para reducir tamaño
-          el.setAttribute('animation', {
-            property: 'scale',
-            to: '0 0 0',
-            dur: 1000,
-            easing: 'easeOutQuad'
-          });
-
-          // Desactivar el objeto y mostrar el texto
-          setTimeout(() => {
-            el.setAttribute('visible', false);
-            textoGanaste.setAttribute('visible', true);
-          }, 1000);
+import LocAR from 'locar';
+console.log('hola mundo')
+document.addEventListener('DOMContentLoaded', () => {
+    // 2. Inicializar LocAR.js
+    const locAR = new LocAR({
+        onLocationUpdate: (coords) => {
+            console.log('Ubicación actual en mi casa:', coords);
+            checkProximity(coords);
         }
-
-      });
-    }
-  });
+    });
+    
+});
